7dd9cb5b39141eb9b7a8b32f636b0cbedcc73ac6
DenysLaptiev <laptiev.denys.18@gmail.com>
2023-11-11 13:26:16 +0200

    commented tests

--- src/test/java/com/javatechie/aws/SpringBootSnsExampleApplicationTests.java
+++ src/test/java/com/javatechie/aws/SpringBootSnsExampleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.javatechie.aws;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBootSnsExampleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.javatechie.aws;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootSnsExampleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
